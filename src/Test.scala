c07cfe45c218a812c60c1498d8f751a43ab858e0
msiegenthaler <msiegenthaler@gmx.net>
2011-09-14 20:44:56 +0200

    more examples in test

--- src/Test.scala
+++ src/Test.scala
@@ -1,5 +1,6 @@
 import actor._
 import execution._
+import annotation._
 
 object Test extends impls.MainActor {
 
@@ -45,12 +46,69 @@ object Test extends impls.MainActor {
     }
   }
 
+  object Duplicator {
+    def apply[T](as: Actor[T]*)(implicit s: ExecutionStrategy = Sequential): Actor[T] = spawnStateless[T] { msg =>
+      as.map(Send(_, msg))
+    }(s)
+  }
+
+  object Counter2 {
+    def apply(): (Actor[Any], Actor[Actor[Int]]) = {
+      val a = spawnPure(body(0))(Pooled)
+      (a.comap(x => Token), a.comap(RequestCount(_)))
+    }
+    private sealed trait Request
+    private object Token extends Request
+    private case class RequestCount(requestor: Actor[Int]) extends Request
+
+    private def body(soFar: Int)(msg: Request): Action[Request] = msg match {
+      case Token => body(soFar + 1) _
+      case RequestCount(r) => r ! soFar
+    }
+  }
+
+  object Periodic extends ActorImplementor {
+    import java.util._
+    private val timer = new Timer
+
+    case class Subscription(actor: Actor[Unit], intervalMs: Int)
+
+    val periodic = new ActorImpl[Subscription] {
+      override def processMessage(sub: Subscription) = {
+        val task = new TimerTask {
+          override def run = {
+            handleAction(sub.actor ! ())
+            ()
+          }
+        }
+        timer.schedule(task, sub.intervalMs, sub.intervalMs)
+      }
+    }
+  }
+
   import ConsoleActors._
-  
+  import Periodic._
+  import Pooled._
+
   def body(args: Array[String]) = {
-    val out: Actor[Int] = SysoutActor.comap("Count is " + _.toString)
-    val counter = CounterActor.spawn(out)
+    val outCount: Actor[Int] = SysoutActor.comap("Character-Count so far is " + _.toString)
+    val outChar: Actor[Char] = SysoutActor.comap("Input was " + _)
+
+    //    val counter = CounterActor.spawn(outCount)
+    val (counter, reqCount) = Counter2()
+
+    val duplicater = Duplicator(counter, outChar)(Pooled)
+
+    val stringSplitter = spawnStateless[String] { msg =>
+      msg.map(Send(duplicater, _))
+    }
+
     val in = SysinActor
-    Send(in, counter)
+
+    val fetchCount = spawnStateless[Unit] { msg =>
+      reqCount ! outCount
+    }
+    
+    (in ! stringSplitter) + (periodic ! Periodic.Subscription(fetchCount, 1000))
   }
 }
