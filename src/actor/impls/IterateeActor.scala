7471a3a1c2fb744b4a53c10731ccb686d85b2d1f
msiegenthaler <msiegenthaler@gmx.net>
2011-09-24 17:31:52 +0200

    Define ActionIteratee and add the utility iteratee 'sendTo'

--- src/actor/impls/IterateeActor.scala
+++ src/actor/impls/IterateeActor.scala
@@ -3,8 +3,26 @@ package impls
 
 import execution.ExecutionStrategy
 import io.iteratee._
+import IterateeFun._
 
 object IterateeActor {
+
+  type ActionIteratee[I] = Iteratee[I, StatelessAction]
+
+  def sendTo[M](to: Actor[M]): Iteratee[M, StatelessAction] = mapping(Send(to, _))
+
+  def apply[I, O](it: ActionIteratee[I])(implicit s: ExecutionStrategy): Actor[I] = {
+    def f(it: ActionIteratee[I])(m: I): Action[I] = {
+      it(Data(m)) match {
+        case nit @ Result(r) =>
+          r continue (f(nit))
+        case nit =>
+          Continue(f(nit))
+      }
+    }
+    spawnPure[I](f(it))(s)
+  }
+
   def fixed[I, O](it: Iteratee[I, O])(forward: Actor[O])(implicit s: ExecutionStrategy): Actor[I] = {
     def f(it: Iteratee[I, O])(m: I): Action[I] = {
       it(Data(m)) match {
@@ -18,15 +36,4 @@ object IterateeActor {
     spawnPure[I](f(it))(s)
   }
 
-  def apply[I, O](it: Iteratee[I, StatelessAction])(implicit s: ExecutionStrategy): Actor[I] = {
-    def f(it: Iteratee[I, StatelessAction])(m: I): Action[I] = {
-      it(Data(m)) match {
-        case nit @ Result(r) =>
-          r continue (f(nit))
-        case nit =>
-          Continue(f(nit))
-      }
-    }
-    spawnPure[I](f(it))(s)
-  }
 }
