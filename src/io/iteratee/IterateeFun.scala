8f5cbdb3f9cac511d169b373fdacc9ea790aebdc
msiegenthaler <msiegenthaler@gmx.net>
2011-09-25 14:02:27 +0200

    add new functions to IterateeFun (unit, take, drop, count)

--- src/io/iteratee/IterateeFun.scala
+++ src/io/iteratee/IterateeFun.scala
@@ -4,6 +4,8 @@ package iteratee
 import Iteratee._
 
 object IterateeFun {
+  def unit[A]: Iteratee[A, A] = mapping(a ⇒ a)
+
   def mapping[A, B](f: A ⇒ B): Iteratee[A, B] = {
     def handle(in: Input[A]): Iteratee[A, B] = in match {
       case Data(d) ⇒ cont(handle, f(d))
@@ -11,6 +13,36 @@ object IterateeFun {
       case EOF     ⇒ done
     }
     cont(handle)
+  }
+
+  def take[A](n: Long): Iteratee[A, A] = {
+    def handle(n: Long)(in: Input[A]): Iteratee[A, A] = in match {
+      case Data(d) if n > 1  ⇒ cont(handle(n - 1), d)
+      case Data(d) if n == 1 ⇒ done(d)
+      case Data(d)           ⇒ done
+      case Empty             ⇒ cont(handle(n))
+      case EOF               ⇒ done
+    }
+    cont(handle(n))
+  }
+
+  def drop[A](n: Long): Iteratee[A, A] = {
+    def handle(n: Long)(in: Input[A]): Iteratee[A, A] = in match {
+      case Data(d) if n > 0 ⇒ cont(handle(n - 1))
+      case Data(d)          ⇒ cont(handle(0), d)
+      case Empty            ⇒ cont(handle(n))
+      case EOF              ⇒ done
+    }
+    cont(handle(n))
+  }
+
+  def count[A]: Iteratee[A, Long] = {
+    def handle(n: Int)(in: Input[A]): Iteratee[A, Long] = in match {
+      case Data(_) ⇒ cont(handle(n + 1), n)
+      case Empty   ⇒ cont(handle(n))
+      case EOF     ⇒ done
+    }
+    cont(handle(0))
   }
 
   def compose[I, A, O](a: Iteratee[I, A], b: Iteratee[A, O]): Iteratee[I, O] = {
