60e9b892c7faebc24897fd9349ab9b3a87ad3a61
msiegenthaler <msiegenthaler@gmx.net>
2011-09-25 14:41:40 +0200

    unified behaviour of + function on action

--- src/actor/Action.scala
+++ src/actor/Action.scala
@@ -2,34 +2,38 @@ package actor
 
 sealed trait Action[+A] {
   def +[M](to: Actor[M], msg: M): Action[A] = this + Send(to, msg)
-  def +[M](s: Send[M]): Action[A]
+  def +(o: StatelessAction): Action[A]
 }
 
 sealed trait StatelessAction extends Action[Nothing] {
   def continue[A](f: ActorFun[A]): StatefulAction[A]
+  def +(o: StatelessAction): StatelessAction
+  private[actor] def msgs: Traversable[Send[_]]
 }
 
 sealed trait StatefulAction[+A] extends Action[A]
 
 object Noop extends StatelessAction {
-  override def +[M](s: Send[M]) = Sends(List(s))
+  override def +(s: StatelessAction) = s
   override def continue[A](f: ActorFun[A]) = Continue(f)
+  private[actor] override def msgs = Nil
 }
 
 case class Send[M](to: Actor[M], msg: M) extends StatelessAction {
-  override def +[M](s: Send[M]) = Sends(List(this, s))
+  override def +(s: StatelessAction): Sends = Sends(List(this) ++ s.msgs)
   override def continue[A](f: ActorFun[A]) = Actions(f, List(this))
+  private[actor] override def msgs = List(this)
 }
 
 case class Sends(msgs: Traversable[Send[_]]) extends StatelessAction {
-  override def +[M](s: Send[M]) = Sends(msgs ++ List(s))
+  override def +(s: StatelessAction): Sends = Sends(msgs ++ s.msgs)
   override def continue[A](f: ActorFun[A]) = Actions(f, msgs)
 }
 
 case class Continue[A](f: ActorFun[A]) extends StatefulAction[A] {
-  override def +[M](s: Send[M]) = Actions(f, List(s))
+  override def +(s: StatelessAction): Actions[A] = Actions(f, s.msgs)
 }
 
 case class Actions[A](f: ActorFun[A], msgs: Traversable[Send[_]]) extends StatefulAction[A] {
-  override def +[M](s: Send[M]) = Actions(f, msgs ++ List(s))
+	override def +(s: StatelessAction): Actions[A] = Actions(f, msgs ++ s.msgs)
 }
